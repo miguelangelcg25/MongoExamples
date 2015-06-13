c58e258fce2541fdb2bf4f5c94ffa5f4c7bf97e5
miguelinux <miguelinux@xubuntu-box>
2015-06-13 18:09:56 -0500

    Mongo
     - CRUD

--- src/main/java/org/mongo/crud/MongoCRUD.java
+++ src/main/java/org/mongo/crud/MongoCRUD.java
@@ -6,6 +6,8 @@ import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 import static com.mongodb.client.model.Filters.*;
 import com.mongodb.client.model.Projections;
+import com.mongodb.client.model.Sorts;
+import com.mongodb.client.model.UpdateOptions;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -19,7 +21,6 @@ import org.bson.conversions.Bson;
 public class MongoCRUD {
 
     //By default it connects to localhost:27017
-
     private static final MongoClient client = new MongoClient();
     private static final MongoDatabase db = client.getDatabase("mongoTest");
     private final MongoCollection<Document> users = db.getCollection("users");
@@ -29,6 +30,9 @@ public class MongoCRUD {
         mongo.insertExample();
         mongo.findExample();
         mongo.findWithFilters();
+        mongo.sort();
+        mongo.updateAndReplace();
+        mongo.delete();
     }
 
     public void insertExample() {
@@ -71,50 +75,120 @@ public class MongoCRUD {
         for (Document d : all) {
             System.out.println(d);
         }
-        
+
         //use it when the collection is too large
         //the cursor is not left open when you exhaust the cursor, but if you 
         //get and exception or break before exhaust the cursor then its left open
         System.out.println("\nFind all with iteration (cursor):");
-        try(MongoCursor<Document> cursor = users.find().iterator()){
-            while(cursor.hasNext()){
+        try (MongoCursor<Document> cursor = users.find().iterator()) {
+            while (cursor.hasNext()) {
                 Document doc = cursor.next();
                 System.out.println(doc);
             }
         }
-        
+
         System.out.println("\nCount:");
         System.out.println(users.count());
 
         System.out.println("--\n");
     }
-    
-    public void findWithFilters(){
+
+    public void findWithFilters() {
         System.out.println("Find with FILTERS:");
         users.drop();
 
         for (int i = 0; i < 5; i++) {
             users.insertOne(new Document("x", i).append("y", i * 10));
         }
-        
+
         //db.users.find({ x:3, y:{"$gt:10, $lt:50}})
-        Bson filter = new Document("x", 3).append("y", new Document("$gt",10).append("$lt", 50));
+        Bson filter = new Document("x", 3).append("y", new Document("$gt", 10).append("$lt", 50));
         Document doc = users.find(filter).first();
         System.out.println(doc);
-        
-        filter = and(eq("x",3), gt("y", 10), lt("y", 50));
+
+        filter = and(eq("x", 3), gt("y", 10), lt("y", 50));
         doc = users.find(filter).first();
         System.out.println(doc);
-        
+
         //excludes x and includes y
-        Bson projections = new Document("x",0).append("y", 1);
+        Bson projections = new Document("x", 0).append("y", 1);
         //exlcudes x and _id
         projections = Projections.exclude("x", "_id");
         //includes y, _id is included by default
         projections = Projections.include("y");
         //includes x, y and exlcudes _id
         projections = Projections.fields(Projections.include("x", "y"), Projections.exclude("_id"));
-        List<Document> list = users.find(filter).projection(projections).into(new ArrayList<Document>());        
+        List<Document> list = users.find(filter).projection(projections).into(new ArrayList<Document>());
     }
 
+    public void sort() {
+        System.out.println("\nSort, limit and skip:");
+        users.drop();
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                users.insertOne(new Document("i", i).append("j", j));
+            }
+        }
+
+        Bson projection = Projections.excludeId();
+        //sorts i ascending and j descending
+        Bson sort = new Document("i", 1).append("j", -1);
+        sort = Sorts.ascending("i", "j");
+        sort = Sorts.orderBy(Sorts.ascending("i"), Sorts.descending("i"));
+
+        List<Document> list = users.find()
+                .sort(sort)
+                .projection(projection)
+                .skip(5)
+                .limit(10)
+                .into(new ArrayList<Document>());
+
+        for (Document d : list) {
+            System.out.println(d);
+        }
+    }
+
+    public void updateAndReplace() {
+        System.out.println("\nUpdate and replace:");
+        users.drop();
+
+        for (int i = 0; i < 8; i++) {
+            users.insertOne(new Document("_id", i).append("i", i));
+        }
+
+        //replace the WHOLE document!!!
+        users.replaceOne(eq("_id", 5), new Document("i", 5).append("x", 20));
+        //update a value
+        users.updateOne(eq("_id", 6), new Document("$set", new Document("i", 66)));
+        //upsert
+        users.updateOne(eq("_id", 9), new Document("$set", new Document("i", 99)),
+                new UpdateOptions().upsert(true));
+        //update many documents
+        users.updateMany(gte("_id", 5), new Document("$inc", new Document("i", 1)));
+
+        List<Document> list = users.find().into(new ArrayList<Document>());
+
+        for (Document d : list) {
+            System.out.println(d);
+        }
+
+    }
+
+    public void delete() {
+        System.out.println("\nUpdate and replace:");
+        users.drop();
+
+        for (int i = 0; i < 8; i++) {
+            users.insertOne(new Document("_id", i).append("i", i));
+        }
+        
+        users.deleteMany(gte("_id", 5));
+        users.deleteOne(eq("_id", 3));
+        
+        List<Document> list = users.find().into(new ArrayList<Document>());
+
+        for (Document d : list) {
+            System.out.println(d);
+        }
+    }
 }
